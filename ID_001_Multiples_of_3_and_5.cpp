dc48216a16c3bf5ce0144bb8ed03d39aa0c7a0f3
seongioi <seongiwang@gmail.com>
2017-05-31 18:17:10 -0500

    Initial commit

---
+++ ID_001_Multiples_of_3_and_5.cpp
@@ -0,0 +1,29 @@
+//
+// Created by seong on 5/31/2017.
+//
+#include <iostream>
+
+//sum of all the multiples of 3 or 5 below ADDTO
+#define ADDTO 1000
+
+int main()
+{
+    int total = 0;
+    for (unsigned int i = 1; i <= ADDTO / 3; i++) {//one loop looping to upper bound of summation
+        total += 3 * i;	//add up all multiples of 3
+
+        //add up all multiple of 5 (less than 1000, because 1000 is divisible by 5)
+        if (i < ADDTO / 5) {
+            total += 5 * i;
+
+            //removes repeats with the summation "15*i", the LCM of 3 and 5
+            //inside the "summation of 5" if statement because it runs less than "ADDTO/3" number of times
+            if (i <= ADDTO / 15)
+                total -= 15 * i;
+        }
+    }
+    //print total
+    std::cout << "Sum of all all the multiples of 3 or 5 below 1000 is:" << total << std::endl;
+    return 0;
+}
+
